dc95bf97532a3d7f2e6c78766aaa38bc4fd52807
k-vasanth09072002 <vasanthvasath74@gmail.com>
2023-01-09 12:06:53 +0530

    next-page

---
+++ next.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+    let password2=document.getElementById("password2").value;
+        
+
+}
